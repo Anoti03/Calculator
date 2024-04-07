30092c67e6b2c1e0be3d19d53ccab4eeaf19d5f5
Anoti03 <yehorpashkevych@gmail.com>
2024-04-07 14:07:54 +0200

    Fixed Bug

--- Assets/Scripts/Calculator.cs
+++ Assets/Scripts/Calculator.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Calculator : MonoBehaviour
 {
-   public float Number1 = 5f;
-    public float Number2 = 3f;
-    public float Result = 0f;
-    public float ResultMinus = 0f;
-    public float ResultMultiply = 0f;
-    public float ResultDivide = 0f;
-    public bool ConsoleNumber;
+   public float Number1 = 0f;
+    public float Number2 = 0f;
+    public float NumberMinus1 = 0f;
+    public float NumberMinus2 = 0f;
+    public float NumberMultiply1 = 0f;
+    public float NumberMultiply2 = 0f;
+    public float NumberDivide1 = 0f;
+    public float NumberDivide2 = 0f;
+    private float Result = 0f;
+    private float ResultMinus = 0f;
+    private float ResultMultiply = 0f;
+    private float ResultDivide = 0f;
+    
    public void Start()
     {
         Result = Number1 + Number2;
-        ResultMinus = Number1 - Number2;
-        ResultMultiply = Number1 * Number2;
-        ResultDivide = Number1 / Number2;
+        ResultMinus = NumberMinus1 - NumberMinus2;
+        ResultMultiply = NumberMultiply1 * NumberMultiply2;
+        ResultDivide = NumberDivide1 / NumberDivide2;
 
 
         Debug.Log(Result);
